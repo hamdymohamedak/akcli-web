6d430ee6788c18051a4a68cc5adacb4146706530
hamdymohamedak <mohameddhamdy407@gmail.com>
2024-08-25 00:53:04 +0300

    add LaRose.js at project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Home from "./Home";
 import Docs from "./Docs";
-
+import { Route, RoseRouter } from "./LaRose";
 export default function App() {
+  // let customStyle = `
+  //                 .route {
+  //                   opacity: 0;
+  //                   transform: translateY(10px);
+  //                   transition: opacity 0.5s ease, transform 0.5s ease;
+  //               }
+  //               .route-active {
+  //                   opacity: 1;
+  //                   transform: translateY(0);
+  //               }
+  // `;
   return (
     <BrowserRouter>
-      <Routes>
+      <RoseRouter>
         <Route path="/" element={<Home />} />
-        <Route path="/Docs" element={<Docs />} />
-      </Routes>
+        <Route path="/docs" element={<Docs />} />
+      </RoseRouter>
     </BrowserRouter>
   );
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import app_file from "./akcli.exe";
+import app_file from "./akcli.exe"; // Ensure this path is correct
 import { Link } from "react-router-dom";
+import { WaveText, SplitText, RoseBox, useRouter } from "./LaRose";
+
 export default function Home() {
+  const [Tag, setTag] = useState(() => WaveText);
+  const [TagDescription, setTagDescription] = useState(() => SplitText);
+  let { navigate } = useRouter()
+  useEffect(() => {
+    if (window.innerWidth <= 682) {
+      setTag(() => "h1");
+      setTagDescription(() => "h2");
+    }
+  }, []);
+
   return (
     <div className="container">
       <div
@@ -11,13 +23,15 @@ export default function Home() {
           window.open("https://github.com/hamdymohamedak/akcli");
         }}
       >
-        visit the Github repository
+        Visit the Github repository
       </div>
-      <h1>Welcome, Developers!</h1>
-      <h2>AKCLI - Your Fast Command Line Tool</h2>
-      <h3>Easily save and retrieve commands with AKCLI</h3>
+      {React.createElement(Tag, null, "Welcome, Developers!")}
+      {React.createElement(TagDescription, { RoseName: "commandLine" }, "AKCLI - Your Fast Command Line Tool")}
+      {React.createElement(TagDescription, { RoseName: "AKCLI" }, "Easily save and retrieve commands with AKCLI")}
       <div style={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
-        <button className="btn"><Link to={"/docs"} style={{color:"black"}}>Get AKCLI</Link></button>
+        <RoseBox autoLayout AutoHandling onClick={() => {
+          navigate("/docs")
+        }} className="btn">Get AKCLI</RoseBox>
         <a href={app_file} download="akCLI.exe" id="btn" className="btn">
           Download
         </a>
